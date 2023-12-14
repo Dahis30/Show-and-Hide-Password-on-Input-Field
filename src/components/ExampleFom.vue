2bfdac5a68fa9a57dc4aff30b687da1b43f483ba
Dahis30 <mohamednadir637@gmail.com>
2023-12-14 16:06:54 +0100

    changes

---
+++ src/components/ExampleFom.vue
@@ -0,0 +1,21 @@
+<template>
+  <label>Your Password here :</label>
+  <input :type="type_input" /> <br /> <br /> <br /> 
+  <button @click="switchf"  style="padding: 15px;color: green; background-color: beige ; border: 1px solid rgb(153, 137, 137);"  >
+    SHOW/HIDE YOUR PASSWORD
+  </button>
+</template>
+<script>
+export default {
+  data: function () {
+    return {
+      type_input: "text",
+    };
+  },
+  methods: {
+    switchf() {
+      this.type_input == "password" ? (this.type_input = "text") : (this.type_input = "password");
+    },
+  },
+};
+</script>
